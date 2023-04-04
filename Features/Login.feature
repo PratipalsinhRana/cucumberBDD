903bb6ed4fd3b098233db18efa90d3819db14423
PratipalsinhRana <pratipalsinh.inexture@gmail.com>
2023-04-04 19:20:18 +0530

    Login Scenario

--- Features/Login.feature
+++ Features/Login.feature
@@ -4,3 +4,28 @@ Feature: Calculator Feature
     When the user enters valid username and password
     And clicks the login button
     Then the user should be redirected to the home page
+
+  Scenario: User can login with invalid credentials
+    Given the user is on the login page
+    When the user enters invalid username and password
+    And clicks the login button
+    Then display error message
+
+  Scenario: User can login with invalid credentials
+    Given the user is on the login page
+    When the user enters invalid username and valid password
+    And clicks the login button
+    Then display error message
+
+  Scenario: User can login with invalid credentials
+    Given the user is on the login page
+    When the user enters valid username and invalid password
+    And clicks the login button
+    Then display error message
+
+  Scenario: User can login with invalid credentials
+    Given the user is on the login page
+    When the user enters empty username and password
+    And clicks the login button
+    Then display error message
+
--- src/test/java/StepDefination/Testing.java
+++ src/test/java/StepDefination/Testing.java
@@ -17,9 +17,11 @@ public class Testing {
 
     public static WebDriver driver;
 
+
     @Given("the user is on the login page")
     public void theUserIsOnTheLoginPage() {
         driver = new FirefoxDriver();
+        driver.manage().window().maximize();
         driver.get("https://testcs.helpr-app.com/");
     }
 
@@ -39,5 +41,45 @@ public class Testing {
     }
     @Then("the user should be redirected to the home page")
     public void theUserShouldBeRedirectedToTheHomePage() {
+    }
+
+    @When("the user enters invalid username and password")
+    public void theUserEntersInvalidUsernameAndPassword() {
+        WebElement email = driver.findElement(By.id("login-email"));
+        WebElement password = driver.findElement(By.id("login-password"));
+        email.sendKeys("a45@notarbitrary.com");
+        password.sendKeys("mGUN7JF7/7Z$f)$v\\K;ke/D$J4RXXzQxNW%;k'6^(C8`f");
+
+    }
+
+    @Then("display error message")
+    public void displayErrorMessage() {
+        WebElement toast = driver.findElement(By.xpath("//div[@class='Toastify']"));
+    }
+
+    @When("the user enters invalid username and valid password")
+    public void theUserEntersInvalidUsernameAndValidPassword() {
+
+        WebElement email = driver.findElement(By.id("login-email"));
+        WebElement password = driver.findElement(By.id("login-password"));
+        email.sendKeys("a1@otarbitrary.com");
+        password.sendKeys("mGUN7JF76UE_9/7Z$f)$v\\K;ke/D$J4RXXzQxNW%;k'6^(C8`f");
+
+    }
+
+    @When("the user enters valid username and invalid password")
+    public void theUserEntersValidUsernameAndInvalidPassword() {
+        WebElement email = driver.findElement(By.id("login-email"));
+        WebElement password = driver.findElement(By.id("login-password"));
+        email.sendKeys("a1@notarbitrary.com");
+        password.sendKeys("mGUN7JF_9/7Z$f)$v\\K;ke/D$J4RXXzQxNW%;k'6^(C8`f");
+
+    }
+
+    @When("the user enters empty username and password")
+    public void theUserEntersEmptyUsernameAndPassword() {
+        WebElement login = driver.findElement(By.xpath("//span[@class='MuiButton-label']"));
+        login.click();
+        driver.close();
     }
 }
